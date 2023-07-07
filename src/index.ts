640d3fb6d5734d9dc73bd2d42f1abeac47b1a6e1
Felipe Torres <felipe.torressepulveda@gmail.com>
2023-07-07 07:47:09 -0700

    update

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,26 @@
-import type { RequestInfo } from "@cloudflare/workers-types";
-import type { CloudflareWorkersHandler } from "@as-integrations/cloudflare-workers";
-import { createGraphQLHandler } from "~/handlers/apollo";
+import { createYoga, createSchema } from "graphql-yoga";
 
-const graphQLOptions: GraphQLOptions = {
-  baseEndpoint: GRAPHQL_BASE_ENDPOINT,
-  kvCache: Boolean(GRAPHQL_KV_CACHE),
-};
-
-const handleGraphQLRequest: CloudflareWorkersHandler =
-  createGraphQLHandler(graphQLOptions);
-
-const handleRequest = async (
-  request: FetchEvent["request"],
-): Promise<Response> => {
-  const { pathname } = new URL(request.url);
-
-  try {
-    if (pathname === graphQLOptions.baseEndpoint) {
-      return handleGraphQLRequest(request);
-    }
-
-    return graphQLOptions?.forwardUnmatchedRequestsToOrigin
-      ? fetch(request)
-      : new Response("Not found", { status: 404 });
-  } catch (err: unknown) {
-    return new Response(
-      graphQLOptions?.debug ? (err as Error).message : "Something went wrong",
-      {
-        status: 500,
+const yoga = createYoga({
+  cors: {
+    origin: ["http://localhost:3000", "https://localhost:3000"],
+    credentials: true,
+    allowedHeaders: ["X-Custom-Header"],
+    methods: ["POST"],
+  },
+  schema: createSchema({
+    typeDefs: /* GraphQL */ `
+      type Query {
+        hello: String!
+      }
+    `,
+    resolvers: {
+      Query: {
+        hello: () => "Hello World!",
       },
-    );
-  }
-};
-
-addEventListener("fetch", (event) => {
-  event.respondWith(handleRequest(event.request));
+    },
+  }),
 });
+export default {
+  // eslint-disable-next-line @typescript-eslint/unbound-method
+  fetch: yoga.fetch,
+};
