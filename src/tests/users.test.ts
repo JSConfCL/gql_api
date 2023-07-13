a1ce937c5dbea1421ec9e66658cedfa44b295835
Felipe Torres <felipe.torressepulveda@gmail.com>
2023-07-13 00:37:30 -0700

    someupdate

--- src/tests/users.test.ts
+++ src/tests/users.test.ts
@@ -1,10 +1,10 @@
-import { it, describe } from "vitest";
+import { it, describe, assert } from "vitest";
 import { insertUser } from "~/tests/seeds";
 
 describe("Users Graphql Tests", () => {
   it("Should return a list of users", async () => {
     const user = await insertUser();
-    console.log(user);
-    return true;
+    const user2 = await insertUser();
+    console.log(user, user2);
   });
 });
