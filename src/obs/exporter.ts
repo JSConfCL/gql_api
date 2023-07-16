dc3d693c73121d086dff464a63811e29902b87c5
Felipe Torres <felipe.torressepulveda@gmail.com>
2023-07-16 10:52:52 -0700

    Adding logging and obs

---
+++ src/obs/exporter.ts
@@ -0,0 +1,112 @@
+import { createExportTraceServiceRequest } from "@opentelemetry/otlp-transformer";
+import {
+  ExportServiceError,
+  OTLPExporterError,
+} from "@opentelemetry/otlp-exporter-base";
+import { ExportResult, ExportResultCode } from "@opentelemetry/core";
+import { ReadableSpan, SpanExporter } from "@opentelemetry/sdk-trace-base";
+import {
+  BasicTracerProvider,
+  SimpleSpanProcessor,
+} from "@opentelemetry/sdk-trace-base";
+
+export interface OTLPExporterConfig {
+  url: string;
+  headers?: Record<string, string>;
+}
+
+const defaultHeaders: Record<string, string> = {
+  accept: "application/json",
+  "content-type": "application/json",
+};
+
+class OTLPExporter implements SpanExporter {
+  private headers: Record<string, string>;
+  private url: string;
+  constructor(config: OTLPExporterConfig) {
+    this.url = config.url;
+    this.headers = Object.assign({}, defaultHeaders, config.headers);
+  }
+
+  export(
+    items: ReadableSpan[],
+    resultCallback: (result: ExportResult) => void,
+  ): void {
+    this._export(items)
+      .then(() => {
+        resultCallback({ code: ExportResultCode.SUCCESS });
+      })
+      .catch((error: ExportServiceError) => {
+        resultCallback({ code: ExportResultCode.FAILED, error });
+      });
+  }
+
+  private _export(items: ReadableSpan[]): Promise<unknown> {
+    return new Promise<void>((resolve, reject) => {
+      try {
+        this.send(items, resolve, reject);
+      } catch (e) {
+        reject(e);
+      }
+    });
+  }
+
+  send(
+    items: ReadableSpan[],
+    onSuccess: () => void,
+    onError: (error: OTLPExporterError) => void,
+  ): void {
+    const exportMessage = createExportTraceServiceRequest(items, true);
+    const body = JSON.stringify(exportMessage);
+    console.log(this.headers);
+    const params: RequestInit = {
+      method: "POST",
+      headers: this.headers,
+      body,
+    };
+
+    fetch(this.url, params)
+      .then((response) => {
+        if (response.ok) {
+          onSuccess();
+        } else {
+          onError(
+            new OTLPExporterError(
+              `Exporter received a statusCode: ${response.status}`,
+            ),
+          );
+        }
+      })
+      .catch((error: OTLPExporterError) => {
+        onError(
+          new OTLPExporterError(
+            `Exception during export: ${error.toString()}`,
+            error.code,
+            error.stack,
+          ),
+        );
+      });
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  async shutdown(): Promise<void> {}
+}
+
+console.log(
+  "HONEYCOMB_TOKEN='",
+  typeof HONEYCOMB_TOKEN !== "undefined" ? HONEYCOMB_TOKEN : "",
+  "'",
+);
+
+const exporter = new OTLPExporter({
+  url: "https://api.honeycomb.io/v1/traces",
+  headers: {
+    "x-honeycomb-team":
+      typeof HONEYCOMB_TOKEN !== "undefined" ? HONEYCOMB_TOKEN : "",
+    "x-honeycomb-dataset": "graphql jschile api",
+  },
+});
+
+export const provider = new BasicTracerProvider();
+provider.addSpanProcessor(new SimpleSpanProcessor(exporter));
+provider.register();
