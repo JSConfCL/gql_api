14f56cb174ee48a839cdc5fb3c53fc4757f68c8c
Benjamin <BPerez51511a@gmail.com>
2023-10-01 20:47:02 -0300

    agregue un helper que agrega propiedades a un objeto si existe

--- src/schema/shared/helpers.ts
+++ src/schema/shared/helpers.ts
@@ -2,3 +2,13 @@ export const sanitizeForLikeSearch = (str: string) => {
   const sanitizedString = str.replace(/[%_]/g, "\\$&");
   return `%${sanitizedString}%`;
 };
+
+export const addToObjectIfPropertyExists = (
+  object: any,
+  key: string,
+  value: number | string | boolean | Date | undefined | null,
+) => {
+  if (value) {
+    object[key] = value;
+  }
+};
