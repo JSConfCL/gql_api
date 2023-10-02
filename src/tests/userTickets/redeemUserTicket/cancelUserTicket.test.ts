3c1903e0d51c029c08298ed9918bd72de3b03b7d
Benjamin <BPerez51511a@gmail.com>
2023-10-02 13:19:37 -0300

    added test

--- src/tests/userTickets/redeemUserTicket/cancelUserTicket.test.ts
+++ src/tests/userTickets/redeemUserTicket/cancelUserTicket.test.ts
@@ -150,6 +150,90 @@ describe("Redeem user ticket", () => {
     assert.equal(response.errors, undefined);
     assert.equal(response.data?.redeemUserTicket?.redemptionStatus, "redeemed");
   });
+  it("Should redeem a user ticket if user is event admin", async () => {
+    const community1 = await insertCommunity();
+    const event1 = await insertEvent();
+    await insertEventToCommunity({
+      eventId: event1.id,
+      communityId: community1.id,
+    });
+    const user1 = await insertUser();
+    await insertUserToCommunity({
+      communityId: community1.id,
+      userId: user1.id,
+      role: "member",
+    });
+    await insertUserToEvent({
+      eventId: event1.id,
+      userId: user1.id,
+      role: "admin",
+    });
+    const ticketTemplate1 = await insertTicketTemplate({
+      eventId: event1.id,
+    });
+    const ticket1 = await insertTicket({
+      ticketTemplateId: ticketTemplate1.id,
+      userId: user1.id,
+      status: "active",
+    });
+    const response = await executeGraphqlOperationAsUser<
+      RedeemUserTicketMutation,
+      RedeemUserTicketMutationVariables
+    >(
+      {
+        document: RedeemUserTicket,
+        variables: {
+          userTicketId: ticket1.id,
+        },
+      },
+      user1,
+    );
+
+    assert.equal(response.errors, undefined);
+    assert.equal(response.data?.redeemUserTicket?.redemptionStatus, "redeemed");
+  });
+  it("Should redeem a user ticket if user is event admin", async () => {
+    const community1 = await insertCommunity();
+    const event1 = await insertEvent();
+    await insertEventToCommunity({
+      eventId: event1.id,
+      communityId: community1.id,
+    });
+    const user1 = await insertUser();
+    await insertUserToCommunity({
+      communityId: community1.id,
+      userId: user1.id,
+      role: "member",
+    });
+    await insertUserToEvent({
+      eventId: event1.id,
+      userId: user1.id,
+      role: "collaborator",
+    });
+    const ticketTemplate1 = await insertTicketTemplate({
+      eventId: event1.id,
+    });
+    const ticket1 = await insertTicket({
+      ticketTemplateId: ticketTemplate1.id,
+      userId: user1.id,
+      status: "active",
+    });
+    const response = await executeGraphqlOperationAsUser<
+      RedeemUserTicketMutation,
+      RedeemUserTicketMutationVariables
+    >(
+      {
+        document: RedeemUserTicket,
+        variables: {
+          userTicketId: ticket1.id,
+        },
+      },
+      user1,
+    );
+
+    assert.equal(response.errors, undefined);
+    assert.equal(response.data?.redeemUserTicket?.redemptionStatus, "redeemed");
+  });
   it("It should throw an error, if ticket does not exist", async () => {
     const community1 = await insertCommunity();
     const event1 = await insertEvent();
