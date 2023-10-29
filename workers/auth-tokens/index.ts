be7ff2920ac57305843ab72a21363abf06829a53
Felipe Torres <felipe.torressepulveda@gmail.com>
2023-10-29 10:37:55 -0700

    tokens

---
+++ workers/auth-tokens/index.ts
@@ -0,0 +1,59 @@
+import { Hono } from "hono";
+import { google, SocialProvider } from "worker-auth-providers";
+import { ENV } from "./types";
+
+type HONO_ENV = {
+  Bindings: ENV;
+};
+
+const app = new Hono<HONO_ENV>();
+
+app.get("/auth/google", async (c) => {
+  const { GOOGLE_CLIENT_ID, GOOGLE_REDIRECT_URL, GOOGLE_CLIENT_SECRET } = c.env;
+  if (!GOOGLE_CLIENT_ID) {
+    throw new Error("Missing GOOGLE_CLIENT_ID");
+  }
+  if (!GOOGLE_REDIRECT_URL) {
+    throw new Error("Missing GOOGLE_REDIRECT_URL");
+  }
+  if (!GOOGLE_CLIENT_SECRET) {
+    throw new Error("Missing GOOGLE_CLIENT_SECRET");
+  }
+  const location = await (google as SocialProvider<string>).redirect({
+    options: {
+      clientId: GOOGLE_CLIENT_ID,
+      redirectTo: GOOGLE_REDIRECT_URL,
+      scope: ["https://www.googleapis.com/auth/photoslibrary"],
+    },
+  });
+
+  return new Response(null, {
+    status: 302,
+    headers: {
+      location,
+    },
+  });
+});
+app.all("/auth/google/success", async (c) => {
+  const { GOOGLE_CLIENT_ID, GOOGLE_REDIRECT_URL, GOOGLE_CLIENT_SECRET } = c.env;
+  if (!GOOGLE_CLIENT_ID) {
+    throw new Error("Missing GOOGLE_CLIENT_ID");
+  }
+  if (!GOOGLE_REDIRECT_URL) {
+    throw new Error("Missing GOOGLE_REDIRECT_URL");
+  }
+  if (!GOOGLE_CLIENT_SECRET) {
+    throw new Error("Missing GOOGLE_CLIENT_SECRET");
+  }
+  const { code } = c.req.query();
+  const { access_token: accessToken } = await (
+    google as SocialProvider<string>
+  ).getTokensFromCode(code, {
+    clientId: GOOGLE_CLIENT_ID,
+    clientSecret: GOOGLE_CLIENT_SECRET,
+    redirectUrl: GOOGLE_REDIRECT_URL,
+  });
+  return c.text(accessToken);
+});
+
+export default app;
