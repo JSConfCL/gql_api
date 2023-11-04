5d59b4b9af7d80ce7cc8e173d07dcd6b3e896797
Felipe Torres <felipe.torressepulveda@gmail.com>
2023-11-04 13:16:25 -0700

    queue consumer

--- workers/google_import_queue_consumer/consumer.tsx
+++ workers/google_import_queue_consumer/consumer.tsx
@@ -15,7 +15,7 @@ const getSanityClient = (env: ENV) =>
     dataset: env.SANITY_DATASET,
     useCdn: false,
     apiVersion: env.SANITY_API_VERSION, // use current date (YYYY-MM-DD) to target the latest API version
-    token: process.env.SANITY_SECRET_TOKEN, // Only if you want to update content with the client
+    token: env.SANITY_SECRET_TOKEN, // Only if you want to update content with the client
   });
 
 export const queueConsumer: ExportedHandlerQueueHandler<
