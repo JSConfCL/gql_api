7b9be3f1d259cc5591e120b10eb6b6656c71740a
Felipe Torres <ftorres@openai.com>
2024-03-18 09:30:12 -0700

    Moving DB outof module

--- src/datasources/db/index.ts
+++ src/datasources/db/index.ts
@@ -4,21 +4,18 @@ import { NeonDatabase, drizzle } from "drizzle-orm/neon-serverless";
 import * as schema from "./schema";
 
 export type ORM_TYPE = NeonDatabase<typeof schema>;
-let db: ORM_TYPE | null = null;
 export const getDb = ({ neonUrl }: { neonUrl: string }) => {
-  if (!db) {
-    const client = new Pool({
-      connectionString: neonUrl,
-    });
-    db = drizzle(client, {
-      schema,
-      logger: {
-        logQuery(query, params) {
-          // eslint-disable-next-line no-console
-          console.log(query, params);
-        },
+  const client = new Pool({
+    connectionString: neonUrl,
+  });
+  const db = drizzle(client, {
+    schema,
+    logger: {
+      logQuery(query, params) {
+        // eslint-disable-next-line no-console
+        console.log(query, params);
       },
-    });
-  }
+    },
+  });
   return db;
 };
