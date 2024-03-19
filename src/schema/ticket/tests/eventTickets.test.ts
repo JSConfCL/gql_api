ea9c3ef6d5d1066b943fdbadcee4fb4ad376c5b1
Felipe Torres <ftorres@openai.com>
2024-03-18 23:04:48 -0700

    update

--- src/schema/ticket/tests/eventTickets.test.ts
+++ src/schema/ticket/tests/eventTickets.test.ts
@@ -16,6 +16,41 @@ import {
   EventTicketsQueryVariables,
 } from "./eventTickets.generated";
 
+const createAllTickets = async (eventId: string) => {
+  const ticket = await insertTicketTemplate({
+    eventId: eventId,
+    status: "active",
+    visibility: "public",
+  });
+  const ticket2 = await insertTicketTemplate({
+    eventId: eventId,
+    status: "active",
+    visibility: "private",
+  });
+  const ticket3 = await insertTicketTemplate({
+    eventId: eventId,
+    status: "active",
+    visibility: "unlisted",
+  });
+  const ticket4 = await insertTicketTemplate({
+    eventId: eventId,
+    status: "inactive",
+    visibility: "public",
+  });
+  const ticket5 = await insertTicketTemplate({
+    eventId: eventId,
+    status: "inactive",
+    visibility: "private",
+  });
+  const ticket6 = await insertTicketTemplate({
+    eventId: eventId,
+    status: "inactive",
+    visibility: "unlisted",
+  });
+
+  return [ticket, ticket2, ticket3, ticket4, ticket5, ticket6] as const;
+};
+
 describe("Should get events and its tickets", () => {
   it("as a User", async () => {
     const user1 = await insertUser({
@@ -27,10 +62,7 @@ describe("Should get events and its tickets", () => {
       eventId: event1.id,
       communityId: community1.id,
     });
-    const ticket = await insertTicketTemplate({
-      eventId: event1.id,
-    });
-
+    const [ticket] = await createAllTickets(event1.id);
     const response = await executeGraphqlOperationAsUser<
       EventTicketsQuery,
       EventTicketsQueryVariables
@@ -52,10 +84,7 @@ describe("Should get events and its tickets", () => {
       eventId: event1.id,
       communityId: community1.id,
     });
-    const ticket = await insertTicketTemplate({
-      eventId: event1.id,
-    });
-
+    const [ticket] = await createAllTickets(event1.id);
     const response = await executeGraphqlOperation<
       EventTicketsQuery,
       EventTicketsQueryVariables
