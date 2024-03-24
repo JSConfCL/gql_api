4024b8528e3c58ce66539af9d3c1ab3e6531319c
Felipe Torres <ftorres@openai.com>
2024-03-23 22:15:45 -0700

    initial_migration

--- src/datasources/queries/users.ts
+++ src/datasources/queries/users.ts
@@ -2,71 +2,61 @@ import { eq, sql } from "drizzle-orm";
 import { z } from "zod";
 
 import { ORM_TYPE } from "~/datasources/db";
-import { selectUsersSchema, usersSchema } from "~/datasources/db/schema";
+import {
+  insertUsersSchema,
+  selectUsersSchema,
+  usersSchema,
+} from "~/datasources/db/schema";
 import { getUsername } from "~/datasources/queries/utils/createUsername";
 
-export const ProfileInfoSchema = z.object({
-  sub: z.string(),
-  email: z.string(),
-  email_verified: z.boolean(),
-  image_url: z.string(),
-  two_factor_enabled: z.boolean(),
-  external_id: z.string().nullable(),
-  name: z.string().default(""),
-  surname: z.string().default(""),
-  unsafe_metadata: z.any().optional(),
-  public_metadata: z.any().optional(),
-});
 export const updateUserProfileInfo = async (
   db: ORM_TYPE,
-  newProfileInfo: z.infer<typeof ProfileInfoSchema>,
+  parsedProfileInfo: z.infer<typeof insertUsersSchema>,
 ) => {
-  const parsedProfileInfo = ProfileInfoSchema.parse(newProfileInfo);
   const result = await db.query.usersSchema.findFirst({
-    where: (u, { eq }) => eq(u.id, parsedProfileInfo.sub),
+    where: (u, { eq }) => eq(u.externalId, parsedProfileInfo.externalId),
   });
   if (!result) {
-    console.log("User not found, creating new user");
+    console.log("User not found — creating new user");
     // we create the user
-    const createdUser = (
-      await db
-        .insert(usersSchema)
-        .values({
-          id: parsedProfileInfo.sub,
-          email: parsedProfileInfo.email,
-          username: getUsername(),
-          name: parsedProfileInfo.name,
-          lastName: parsedProfileInfo.surname,
-          twoFactorEnabled: parsedProfileInfo.two_factor_enabled,
-          imageUrl: parsedProfileInfo.image_url,
-          emailVerified: parsedProfileInfo.email_verified,
-          unsafeMetadata: parsedProfileInfo.unsafe_metadata ?? {},
-          publicMetadata: parsedProfileInfo.public_metadata ?? {},
-          updatedAt: sql`current_timestamp`,
-        })
-        .returning()
-    )?.[0];
+    const createdUsers = await db
+      .insert(usersSchema)
+      .values({
+        externalId: parsedProfileInfo.externalId,
+        email: parsedProfileInfo.email,
+        username: parsedProfileInfo.username ?? getUsername(),
+        name: parsedProfileInfo.name,
+        imageUrl: parsedProfileInfo.imageUrl,
+        emailVerified: parsedProfileInfo.emailVerified,
+        publicMetadata: parsedProfileInfo.publicMetadata ?? {},
+      })
+      .returning();
+    const createdUser = createdUsers?.[0];
+    if (!createdUser) {
+      console.error("Could not create user");
+      throw new Error("Could not create user");
+    }
 
     return selectUsersSchema.parse(createdUser);
   } else {
-    console.log("User found for updating user");
+    console.log("User found — updating user");
     // we update the user
-    const updatedUser = (
-      await db
-        .update(usersSchema)
-        .set({
-          name: parsedProfileInfo.name,
-          lastName: parsedProfileInfo.surname,
-          twoFactorEnabled: parsedProfileInfo.two_factor_enabled,
-          imageUrl: parsedProfileInfo.image_url,
-          emailVerified: parsedProfileInfo.email_verified,
-          unsafeMetadata: parsedProfileInfo.unsafe_metadata ?? {},
-          publicMetadata: parsedProfileInfo.public_metadata ?? {},
-          updatedAt: sql`current_timestamp`,
-        })
-        .where(eq(usersSchema.id, parsedProfileInfo.sub))
-        .returning()
-    )?.[0];
+    const updatedUsers = await db
+      .update(usersSchema)
+      .set({
+        name: parsedProfileInfo.name,
+        imageUrl: parsedProfileInfo.imageUrl,
+        emailVerified: parsedProfileInfo.emailVerified,
+        publicMetadata: parsedProfileInfo.publicMetadata ?? {},
+        updatedAt: sql`current_timestamp`,
+      })
+      .where(eq(usersSchema.externalId, parsedProfileInfo.externalId))
+      .returning();
+    const updatedUser = updatedUsers?.[0];
+    if (!updatedUser) {
+      console.error("Could not update user");
+      throw new Error("Could not update user");
+    }
     return selectUsersSchema.parse(updatedUser);
   }
 };
