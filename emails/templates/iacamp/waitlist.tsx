4b25978572b6cfa846b81a55282a347ed4324aaa
Hector Vergara <hvergara@gmail.com>
2024-07-08 14:00:52 -0400

    Update webinar details

--- emails/templates/iacamp/waitlist.tsx
+++ emails/templates/iacamp/waitlist.tsx
@@ -1,4 +1,12 @@
-import { Container, Hr, Preview, Section, Text } from "@react-email/components";
+import {
+  Button,
+  Container,
+  Hr,
+  Link,
+  Preview,
+  Section,
+  Text,
+} from "@react-email/components";
 import * as React from "react";
 
 import { BigFooter, TicketTemplate } from "emails/helpers/tickets";
@@ -21,19 +29,40 @@ export const IACampWaitlist = ({ nombre }: AddedToWaitlist) => {
           {nombre && <Text className="text-xl mb-4">Hey {nombre}!</Text>}
 
           <Text className="text-xl mb-8">
-            Confirmamos tu registro al webinar que tendremos el próximo 8 de
-            julio a las 6:00 pm (hora Santiago, Chile). Antes del evento te
-            enviaremos el link de acceso.
+            Confirmamos tu registro al{" "}
+            <Link href="https://www.youtube.com/live/RSNRrqbLR84?si=DnG257y3MyWDppbi">
+              webinar
+            </Link>{" "}
+            que tendremos este lunes 8 de julio a las 6:00 pm hora Santiago,
+            Chile (GMT-4).
           </Text>
 
           <Text className="text-xl mb-8">
-            Durante este webinar resolveremos todas tus dudas y abriremos las
+            En esta actividad resolveremos todas tus dudas y abriremos las
             postulaciones para participar en la primera hackathon de OpenAI en
             Latinoamérica.
           </Text>
 
-          <Text className="text-xl">Un saludo,</Text>
-          <Text className="text-xl mb-8">Equipo CommunityOS</Text>
+          <Text className="text-center mb-8">
+          <Button
+            href="https://www.youtube.com/live/RSNRrqbLR84?si=DnG257y3MyWDppbi"
+            className="bg-blue-800 py-4 px-6 rounded-md text-gray-200 self-center"
+            target="_blank"
+          >
+            Entrar al Webinar
+          </Button>
+          </Text>
+
+          <Text className="text-xl mb-8">
+            Agrega{" "}
+            <Link href="https://www.youtube.com/live/RSNRrqbLR84?si=DnG257y3MyWDppbi">
+              este evento a tu calendario
+            </Link>{" "}
+            para no olvidarlo.
+          </Text>
+
+          <Text className="text-xl">Un saludo,<br/> Equipo CommunityOS</Text>
+          <Text className="text-xl mb-8"></Text>
         </Section>
         <Hr className="my-8" />
         <BigFooter />
