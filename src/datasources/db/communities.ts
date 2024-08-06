5086543f690011f929897ccae8eb7183fd693d2b
felipe torres <felipe.torressepulveda@gmail.com>
2024-08-05 22:15:19 -0700

    Adding a resend helper to save/update audiences

--- src/datasources/db/communities.ts
+++ src/datasources/db/communities.ts
@@ -21,6 +21,7 @@ export const communitySchema = pgTable("communities", {
   bannerImageSanityRef: text("banner_image_sanity_ref"), // these are sanity image IDs
   paymentSuccessRedirectURL: text("payment_success_redirect_url"),
   paymentCancelRedirectURL: text("payment_cancel_redirect_url"),
+  resendAudienceId: text("resend_audience_id"),
   status: text("status", { enum: communityStatusEnum })
     .default("inactive")
     .notNull(),
@@ -36,4 +37,14 @@ export const communityRelations = relations(communitySchema, ({ many }) => ({
 export const selectCommunitySchema = createSelectSchema(communitySchema);
 export const insertCommunitySchema = createInsertSchema(communitySchema, {
   name: (schema) => schema.name.min(2).max(64),
+});
+export const updateCommunitySchema = insertCommunitySchema.pick({
+  name: true,
+  description: true,
+  status: true,
+  logoImageSanityRef: true,
+  bannerImageSanityRef: true,
+  paymentSuccessRedirectURL: true,
+  paymentCancelRedirectURL: true,
+  resendAudienceId: true,
 });
