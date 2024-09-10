65d1326ddf6290d8448a59fc3f6e0d802f191ca9
Ignacio Guzmán <ignacio658mg@gmail.com>
2024-09-10 12:39:06 -0300

    chore: remove unused variable in createOrUpdateStripeProductAndPrice mock

--- src/datasources/stripe/__mocks__/index.ts
+++ src/datasources/stripe/__mocks__/index.ts
@@ -1,20 +1,8 @@
 import { faker } from "@faker-js/faker";
 
-export const createOrUpdateStripeProductAndPrice = async ({
-  item,
-}: {
-  item: {
-    id: string;
-    currency: string;
-    name: string;
-    description?: string;
-    metadata?: {
-      [name: string]: string | number | null;
-    };
-    unit_amount: number;
+export const createOrUpdateStripeProductAndPrice =
+  async (): Promise<string> => {
+    return new Promise((resolve) => {
+      resolve(faker.string.uuid());
+    });
   };
-  getStripeClient: () => unknown;
-  // eslint-disable-next-line @typescript-eslint/require-await
-}): Promise<string> => {
-  return faker.string.uuid();
-};
