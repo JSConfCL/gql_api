8e137d2cc99d7a16a0b9213eac5016ae7ee5a217
Felipe Torres <felipe.torressepulveda@gmail.com>
2024-12-02 13:26:38 -0300

    whoops

--- src/schema/ticketAddons/helpers/createUpdateAddonMutationHelpers.ts
+++ src/schema/ticketAddons/helpers/createUpdateAddonMutationHelpers.ts
@@ -334,7 +334,7 @@ const constraintHelpers = {
   }): Promise<void> => {
     const updateSql = sql`
       UPDATE ${addonConstraintsSchema}
-      SET 
+      SET
         ${sql.raw(
           addonConstraintsSchema.relatedAddonId.name,
         )} = tmp.related_addon_id::uuid,
@@ -351,7 +351,7 @@ const constraintHelpers = {
             sql.raw(","),
           )}
       ) AS tmp (id, related_addon_id, constraint_type)
-      WHERE 
+      WHERE
         ${addonConstraintsSchema}.${sql.raw(
           addonConstraintsSchema.id.name,
         )} = tmp.id
